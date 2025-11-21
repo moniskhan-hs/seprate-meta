b06007dc1b61779682febe513f2aa6f90f94b24f
monis_khan <monisykhan@gmail.com>
2025-11-21 15:56:57 +0530

    fixe the depens

--- src/Components/Website.jsx
+++ src/Components/Website.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "@dr.pogodin/react-helmet";
 import axios from "axios";
 
 const Website = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from '@dr.pogodin/react-helmet';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
